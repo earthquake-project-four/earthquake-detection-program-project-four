b520ab5e970c12915b47e2c76d31a0916c97b727
Jess Shi <jess.shi@mail.utoronto.ca>
2023-05-22 12:23:45 -0400

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
-        <>
-        </>
+        <footer>
+            <p>&copy; 2023 <a href="https://gerardjuan.com">Gerard</a>, <a href="https://gagandeep-portfolio.com/">Gagandeep</a>, <a href="https://www.sokimcodes.com/">Kim</a>, <a href="https://jesscodes.dev">Jess</a></p>
+            <p className="juno">Created with love at Juno College</p>
+        </footer>
     )
 };
 

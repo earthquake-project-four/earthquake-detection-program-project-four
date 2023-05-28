ca8075345e7ba7d4005e0021a0822a91ad647bf5
Gerard Juan <gerardandrejuan@gmail.com>
2023-05-28 19:26:24 -0400

    added styling and animation to sidebar

--- src/components/Magnitude.js
+++ src/components/Magnitude.js
@@ -1,37 +1,89 @@
-import Event from "./Event";
-import { useState } from "react";
+import Event from './Event';
+import {useState} from 'react';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import styled from 'styled-components';
 
-const Magnitude = ({ numOfEvents, range, hero, intensity, earthquakeData }) => {
-    const [displayEvents, setDisplayEvents] = useState(false);
-    const filteredData = earthquakeData.filter(earthquake => earthquake.intensity === intensity);
-    const handleClick = () => {
-        setDisplayEvents(!displayEvents);
-    }
+const EventList = styled.ul`
+	padding: 5px 0;
 
-    return (
-        <li>
-            <button onClick={handleClick}>
-                <div className={`legend-dot ${intensity}`}>
-                    <p>{numOfEvents}</p>
-                </div>
-                <div className="legend-text">
-                    <p>{hero}</p>
-                    <p>{range}</p>
-                </div>
-            </button>
-            {
-                displayEvents ? (
-                    <ul>
-                        {
-                            filteredData.map(earthquake => {
-                                return <Event key={earthquake.id} place={earthquake.place} time={earthquake.time} mag={earthquake.mag} />;
-                            })
-                        }
-                    </ul>
-                ) : null
-            }
-        </li>
-    );
+	.event-enter {
+		opacity: 0;
+		transform: translateY(-10px);
+	}
+
+	.event-enter-active {
+		opacity: 1;
+		transform: translateY(0);
+		transition: opacity 500ms, transform 500ms;
+	}
+
+	.event-exit {
+		opacity: 1;
+		transform: translateY(0);
+	}
+
+	.event-exit-active {
+		opacity: 0;
+		transform: translateY(10px);
+		transition: opacity 300ms, transform 300ms;
+	}
+`;
+
+const Magnitude = ({numOfEvents, range, hero, intensity, colour, image, earthquakeData}) => {
+	const [displayEvents, setDisplayEvents] = useState(false);
+	const filteredData = earthquakeData.filter((earthquake) => earthquake.intensity === intensity);
+
+	const handleClick = () => {
+		setDisplayEvents(!displayEvents);
+	};
+
+	return (
+		<li className="legend-list-item">
+			<button
+				style={{backgroundColor: colour}}
+				className={`legend-btn legend-btn-${intensity}`}
+				onClick={handleClick}
+			>
+				<div className="legend-image-container">
+					<img
+						src={image}
+						alt={`${hero} icon`}
+					/>
+				</div>
+				<div className="legend-text">
+					<p>{hero}</p>
+					<p>Magnitude: {range}</p>
+					<p>{numOfEvents} earthquakes</p>
+				</div>
+			</button>
+
+			<TransitionGroup component={EventList}>
+				{displayEvents &&
+					filteredData.length > 0 &&
+					filteredData.map((earthquake) => (
+						<CSSTransition
+							key={earthquake.id}
+							timeout={300}
+							classNames="event"
+						>
+							<Event
+								place={earthquake.place}
+								time={earthquake.time}
+								mag={earthquake.mag}
+							/>
+						</CSSTransition>
+					))}
+				{displayEvents && filteredData.length === 0 && (
+					<CSSTransition
+						timeout={300}
+						classNames="event"
+					>
+						<p>No events for {hero} to attend to at this time!</p>
+					</CSSTransition>
+				)}
+			</TransitionGroup>
+		</li>
+	);
 };
 
 export default Magnitude;

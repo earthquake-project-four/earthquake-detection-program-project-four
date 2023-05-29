08abfab5c989a8677260083457756a5d96f79009
Gerard Juan <gerardandrejuan@gmail.com>
2023-05-29 16:38:14 -0400

    made sidebar more responsive, swapped out hero images, and added state to toggle chevron

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,135 +1,122 @@
-import LoadingAnimation from "./LoadingAnimation";
-import Error from "./Error";
-import Sidebar from "./Sidebar";
-import Map from "./Map";
-import axios from "axios";
-import app from "../firebase/firebase";
-import { onValue, getDatabase, ref, set } from "firebase/database";
-import { useState, useEffect } from "react";
+import LoadingAnimation from './LoadingAnimation';
+import Error from './Error';
+import Sidebar from './Sidebar';
+import Map from './Map';
+import axios from 'axios';
+import app from '../firebase/firebase';
+import {onValue, getDatabase, ref, set} from 'firebase/database';
+import {useState, useEffect} from 'react';
 
-const Main = ({ displaySidebar }) => {
-    const [earthquakeData, setEarthquakeData] = useState([]);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(true);
-    const [newEvents, setNewEvents] = useState({
-        generalGeologyTeachers: [],
-        richMortal: [],
-        strongGood: [],
-        all: [],
-    });
+const Main = ({displaySidebar}) => {
+	const [earthquakeData, setEarthquakeData] = useState([]);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [newEvents, setNewEvents] = useState({
+		generalGeologyTeachers: [],
+		richMortal: [],
+		strongGood: [],
+		all: [],
+	});
 
-    useEffect(() => {
-        const currentDate = new Date();
-        const startTime = new Date(
-            currentDate.getTime() - 24 * 60 * 60 * 1000
-        ).toISOString();
+	useEffect(() => {
+		const currentDate = new Date();
+		const startTime = new Date(currentDate.getTime() - 24 * 60 * 60 * 1000).toISOString();
 
-        axios({
-            url: "https://earthquake.usgs.gov/fdsnws/event/1/query",
-            params: {
-                format: "geojson",
-                starttime: startTime,
-                minmagnitude: "2.5",
-            },
-        })
-            .then((res) => {
-                const results = res.data.features;
-                const eventsLog = {
-                    generalGeologyTeachers: [],
-                    richMortal: [],
-                    strongGood: [],
-                    all: [],
-                };
-                const arrayOfEarthquakes = results.map((earthquake) => {
-                    let colour, intensity;
-                    const mag = earthquake.properties.mag;
-                    if (mag < 3.5) {
-                        colour = "#fddd59";
-                        intensity = "low";
-                        eventsLog.generalGeologyTeachers.push(earthquake.id);
-                    } else if (mag < 6) {
-                        colour = "#ff914d";
-                        intensity = "medium";
-                        eventsLog.richMortal.push(earthquake.id);
-                    } else if (mag < 7) {
-                        colour = "#ff3131";
-                        intensity = "high";
-                        eventsLog.strongGood.push(earthquake.id);
-                    } else {
-                        colour = "#a51b1b";
-                        intensity = "severe";
-                        eventsLog.all.push(earthquake.id);
-                    }
+		axios({
+			url: 'https://earthquake.usgs.gov/fdsnws/event/1/query',
+			params: {
+				format: 'geojson',
+				starttime: startTime,
+				minmagnitude: '2.5',
+			},
+		})
+			.then((res) => {
+				const results = res.data.features;
+				const eventsLog = {
+					generalGeologyTeachers: [],
+					richMortal: [],
+					strongGood: [],
+					all: [],
+				};
+				const arrayOfEarthquakes = results.map((earthquake) => {
+					let colour, intensity;
+					const mag = earthquake.properties.mag;
+					if (mag < 3.5) {
+						colour = '#fddd59';
+						intensity = 'low';
+						eventsLog.generalGeologyTeachers.push(earthquake.id);
+					} else if (mag < 6) {
+						colour = '#ff914d';
+						intensity = 'medium';
+						eventsLog.richMortal.push(earthquake.id);
+					} else if (mag < 7) {
+						colour = '#ff3131';
+						intensity = 'high';
+						eventsLog.strongGood.push(earthquake.id);
+					} else {
+						colour = '#a51b1b';
+						intensity = 'severe';
+						eventsLog.all.push(earthquake.id);
+					}
 
-                    return {
-                        id: earthquake.id,
-                        lat: earthquake.geometry.coordinates[1],
-                        lng: earthquake.geometry.coordinates[0],
-                        mag: mag,
-                        title: earthquake.properties.title,
-                        place: earthquake.properties.place,
-                        time: new Date(
-                            earthquake.properties.time
-                        ).toLocaleString(),
-                        colour: colour,
-                        intensity: intensity,
-                    };
-                });
-                setEarthquakeData(arrayOfEarthquakes);
-                setNewEvents(eventsLog);
-                setTimeout(() => {
-                    setLoading(false);
-                }, 1500);
-            })
-            .catch(() => {
-                setError(true);
-                setTimeout(() => {
-                    setLoading(false);
-                }, 1500);
-            });
-    }, []);
+					return {
+						id: earthquake.id,
+						lat: earthquake.geometry.coordinates[1],
+						lng: earthquake.geometry.coordinates[0],
+						mag: mag,
+						title: earthquake.properties.title,
+						place: earthquake.properties.place,
+						time: new Date(earthquake.properties.time).toLocaleString(),
+						colour: colour,
+						intensity: intensity,
+					};
+				});
+				setEarthquakeData(arrayOfEarthquakes);
+				setNewEvents(eventsLog);
+				setTimeout(() => {
+					setLoading(false);
+				}, 1500);
+			})
+			.catch(() => {
+				setError(true);
+				setTimeout(() => {
+					setLoading(false);
+				}, 1500);
+			});
+	}, []);
 
-    useEffect(() => {
-        const database = getDatabase(app);
-        const dbRef = ref(database, "/testing2");
+	useEffect(() => {
+		const database = getDatabase(app);
+		const dbRef = ref(database, '/testing2');
 
-        onValue(dbRef, (dbResponse) => {
-            if (dbResponse.exists()) {
-                const dataFromFirebase = dbResponse.val();
-                const totalEvents = {};
-                for (let key in dataFromFirebase) {
-                    totalEvents[key] = [
-                        ...new Set([
-                            ...dataFromFirebase[key],
-                            ...newEvents[key],
-                        ]),
-                    ];
-                }
-                set(dbRef, totalEvents);
-            } else {
-                set(dbRef, newEvents);
-            }
-        });
-    }, [newEvents]);
-    return (
-        <main>
-            {
-                loading ? <LoadingAnimation /> : null
-            }
-            {
-                error ? <Error /> : (
-                    <>
-                        {displaySidebar && (
-                            <Sidebar earthquakeData={earthquakeData} />
-                        )}
-                        <div className="map-container">
-                            <Map earthquakeData={earthquakeData} />
-                        </div>
-                    </>
-                )
-            }
-        </main>
-    );
+		onValue(dbRef, (dbResponse) => {
+			if (dbResponse.exists()) {
+				const dataFromFirebase = dbResponse.val();
+				const totalEvents = {};
+				for (let key in dataFromFirebase) {
+					totalEvents[key] = [...new Set([...dataFromFirebase[key], ...newEvents[key]])];
+				}
+				set(dbRef, totalEvents);
+			} else {
+				set(dbRef, newEvents);
+			}
+		});
+	}, [newEvents]);
+	return (
+		<main>
+			{loading ? <LoadingAnimation /> : null}
+			{error ? (
+				<Error />
+			) : (
+				<>
+					{displaySidebar && <Sidebar earthquakeData={earthquakeData} />}
+					<div className="map-container">
+						<Map earthquakeData={earthquakeData} />
+					</div>
+				</>
+			)}
+		</main>
+	);
 };
 
 export default Main;

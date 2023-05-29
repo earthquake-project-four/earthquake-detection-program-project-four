b7c8d3c2e3cb47e86393cfb33f1fe11f08483f5e
Jess Shi <jess.shi@mail.utoronto.ca>
2023-05-29 17:03:22 -0400

    Added about section to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,27 @@
 import Legend from './Legend';
-const Sidebar = ({earthquakeData}) => {
+import About from './About';
+import { useState } from 'react';
+
+const Sidebar = ({ earthquakeData }) => {
+    const [sidebarContent, setSidebarContent] = useState("earthquakes");
+    const handleClick = (content) => {
+        setSidebarContent(content);
+    }
+
 	return (
-		<section className="sidebar">
+        <section className="sidebar">
+            <nav>
+                <ul className="sidebar-nav">
+                    <li><button onClick={() => {handleClick("earthquakes")}}>Earthquakes</button></li>
+                    <li><button onClick={() => {handleClick("about")}}>About</button></li>
+                </ul>
+            </nav>
 			<h1>Earthquake Detection Program</h1>
 			<h2>Magnitude 2.5+ Earthquakes</h2>
-			<p>Past 24 Hours: {earthquakeData.length} earthquakes.</p>
-			<Legend earthquakeData={earthquakeData} />
+            <p>Past 24 Hours: {earthquakeData.length} earthquakes.</p>
+            {
+                sidebarContent === "earthquakes" ? <Legend earthquakeData={earthquakeData} /> : <About />
+            }
 		</section>
 	);
 };

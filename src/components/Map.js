5bda8130fb98df4c20f20f2fcb5a32f84847b2f3
Gagandeep <igagandeep95@gmail.com>
2023-05-30 13:11:22 -0400

    fixed map scrolling

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,37 +1,43 @@
-import MapMarker from './MapMarker';
-import {MapContainer, TileLayer, ZoomControl} from 'react-leaflet';
+import MapMarker from "./MapMarker";
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
 
-const Map = ({earthquakeData}) => {
-	return (
-		<section>
-			<MapContainer
-				center={[28, -68]}
-				zoom={1.5}
-				minZoom={1}
-				maxZoom={6}
-				zoomControl={false}
-				className="map"
-			>
-				<TileLayer
-					url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-					attribution='Map data &copy; <a href="https://carto.com/">CartoDB</a>'
-				/>
-				<ZoomControl position="topright" />
-				{earthquakeData.map((earthquake) => {
-					return (
-						<MapMarker
-							key={earthquake.id}
-							coords={[earthquake.lat, earthquake.lng]}
-							radius={earthquake.mag * 1.5}
-							colour={earthquake.colour}
-							title={earthquake.title}
-							time={earthquake.time}
-						/>
-					);
-				})}
-			</MapContainer>
-		</section>
-	);
+const Map = ({ earthquakeData }) => {
+    const southWest = [-90, -180]; // Minimum latitude and longitude
+    const northEast = [90, 180]; // Maximum latitude and longitude
+    const bounds = [southWest, northEast];
+    return (
+        <section>
+            <MapContainer
+                center={[28, -68]}
+                zoom={1.5}
+                minZoom={1}
+                maxZoom={6}
+                zoomControl={false}
+                bounds={bounds}
+                maxBounds={bounds}
+                maxBoundsViscosity={1.0}
+                className="map"
+            >
+                <TileLayer
+                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    attribution='Map data &copy; <a href="https://carto.com/">CartoDB</a>'
+                />
+                <ZoomControl position="topright" />
+                {earthquakeData.map((earthquake) => {
+                    return (
+                        <MapMarker
+                            key={earthquake.id}
+                            coords={[earthquake.lat, earthquake.lng]}
+                            radius={earthquake.mag * 1.5}
+                            colour={earthquake.colour}
+                            title={earthquake.title}
+                            time={earthquake.time}
+                        />
+                    );
+                })}
+            </MapContainer>
+        </section>
+    );
 };
 
 export default Map;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,47 @@
-import Legend from './Legend';
-import About from './About';
-import { useState } from 'react';
+import Legend from "./Legend";
+import About from "./About";
+import { useState } from "react";
 
 const Sidebar = ({ earthquakeData }) => {
     const [sidebarContent, setSidebarContent] = useState("earthquakes");
     const handleClick = (content) => {
         setSidebarContent(content);
-    }
+    };
 
-	return (
+    return (
         <section className="sidebar">
             <nav>
                 <ul className="sidebar-nav">
-                    <li><button onClick={() => {handleClick("earthquakes")}}>Earthquakes</button></li>
-                    <li><button onClick={() => {handleClick("about")}}>About</button></li>
+                    <li>
+                        <button
+                            onClick={() => {
+                                handleClick("earthquakes");
+                            }}
+                        >
+                            Earthquakes
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={() => {
+                                handleClick("about");
+                            }}
+                        >
+                            About
+                        </button>
+                    </li>
                 </ul>
             </nav>
-			<h1>Earthquake Detection Program</h1>
-			<h2>Magnitude 2.5+ Earthquakes</h2>
+            <h1>Earthquake Detection Program</h1>
+            <h2>Magnitude 2.5+ Earthquakes</h2>
             <p>Past 24 Hours: {earthquakeData.length} earthquakes.</p>
-            {
-                sidebarContent === "earthquakes" ? <Legend earthquakeData={earthquakeData} /> : <About />
-            }
-		</section>
-	);
+            {sidebarContent === "earthquakes" ? (
+                <Legend earthquakeData={earthquakeData} />
+            ) : (
+                <About />
+            )}
+        </section>
+    );
 };
 
 export default Sidebar;
